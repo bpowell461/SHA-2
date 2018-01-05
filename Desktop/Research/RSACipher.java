05067eb5f21a070bee27cbe24a15300d8d6c829e
bpowell461 <35017339+bpowell461@users.noreply.github.com>
2018-01-05 18:01:05 -0500

    Working Implementation of RSA Class

--- Desktop/Research/RSACipher.java
+++ Desktop/Research/RSACipher.java
@@ -4,42 +4,46 @@ import java.math.BigInteger;
 public class RSACipher extends EncryptionFundamental
 {
  private int keyLength = 1024;
- private BigInteger p;
- private BigInteger q;
  private BigInteger n;
  private BigInteger phi;
- private BigInteger e,d; 
+ private BigInteger e; 
+ private BigInteger p, q, d;
  
-  protected RSACipher()
+  public RSACipher()
   {
     generateKeys(keyLength);
   }
-  protected void generateKeys(int keyLength)
+  private void generateKeys(int keyLength)
   {
     p = BigInteger.probablePrime((keyLength/2), randomNum);
     q = BigInteger.probablePrime((keyLength/2), randomNum);
     n = p.multiply(q);
     phi = lcm(p.subtract(BigInteger.ONE), q.subtract(BigInteger.ONE));
     e = new BigInteger("65537");
-    /*do{
-      e.add(BigInteger.ONE);
-    }while(gcd(e, phi)!= BigInteger.ONE);*/
     d = e.modInverse(phi);
   }
-  protected BigInteger encrpyt(String message)
+  public BigInteger encrpyt(String message, BigInteger e, BigInteger n)
   {
    BigInteger messageInt = StringToInt(message);
-   /*do{
-      generateKeys(keyLength);
-   }while(n.compareTo(messageInt) == -1);*/
    return(messageInt.modPow(e, n));
-   }
-   protected String decrypt(BigInteger encryptedMessage)
+  }
+   public String decrypt(BigInteger encryptedMessage, BigInteger d, BigInteger n)
    {
      encryptedMessage = encryptedMessage.modPow(d, n);
      return(IntToString(encryptedMessage));
-     
    }
+  public BigInteger getPublicExp()
+   {
+     return(e);
+   }
+    public BigInteger getPrivateExp()
+   {
+     return(d);
+   }
+  public BigInteger getModulus()
+  {
+    return(n);
+  }
 }
    
       
